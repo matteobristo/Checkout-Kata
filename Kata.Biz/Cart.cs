cfea40de0f3cbc4486f00d0453491d904ed4d68b
Matt Bristo <mjbristo@btinternet.com>
2022-04-05 10:22:01 +0100

    Part 2 adding up basket

--- Kata.Biz/Cart.cs
+++ Kata.Biz/Cart.cs
@@ -9,10 +9,22 @@ namespace Kata.Biz
     public class Cart
     {
         public List<Item> CartItems { get; private set; } = new List<Item>();
+        public decimal Total { get; private set; } = 0M;
 
         public void AddToCart(Item item)
         {
             CartItems.Add(item);
+            CalculateTotal();
+        }
+
+        private void CalculateTotal()
+        {
+            this.Total = 0;
+
+            foreach (Item item in CartItems)
+            {
+                Total += item.UnitPrice;
+            }
         }
     }
 }
---
+++ Kata.Tests/CalculateCostOfCartTests.cs
@@ -0,0 +1,59 @@
+﻿using Kata.Biz;
+using Xunit;
+
+namespace Kata.Tests
+{
+    public class CalculateCostOfCartTests
+    {
+
+        [Fact]
+        public void CalculateCostOfEmptyCart()
+        {
+            var cart = new Cart();
+
+            Assert.Equal(0, cart.Total);
+        }
+
+        [Fact]
+        public void CalculateCostOfSingleProduct()
+        {
+            
+            var cart = new Cart();
+
+            var item1 = new Item('A', 10);
+
+            cart.AddToCart(item1);
+
+            Assert.Equal(10, cart.Total);
+        }
+
+        [Fact]
+        public void CalculateCostOfTwoProducts()
+        {
+            
+            var cart = new Cart();
+
+            var item1 = new Item('A', 10);
+            var item2 = new Item('B', 15);
+
+            cart.AddToCart(item1);
+            cart.AddToCart(item2);
+
+            Assert.Equal(25, cart.Total);
+        }
+
+        [Fact]
+        public void CalculateCostOfMultipleProductsWithPence()
+        {
+            var cart = new Cart();
+
+            var item1 = new Item('A', 10.25M);
+            var item2 = new Item('B', 15.50M);
+
+            cart.AddToCart(item1);
+            cart.AddToCart(item2);
+
+            Assert.Equal(25.75M, cart.Total);
+        }
+    }
+}
